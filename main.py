f0928b68d0f0bb2117623ab07e8cec2cfac7fd80
oblivion4us <oblivion4us@icloud.com>
2024-11-11 15:53:37 +0200

    update main.py and add code in index.html

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import requests
+from googletrans import Translator
 
 app = Flask(__name__)
 
 API_KEY = '/UHpzZen0n1NCOoXbB7SsQ==YAYK376econUC2yi'
+
+# Инициализация переводчика
+translator = Translator()
+
 
 @app.route('/')
 def index():
@@ -18,12 +23,20 @@ def index():
         if quote_data:
             quote = quote_data[0]['quote']
             author = quote_data[0]['author']
-        else:
-            quote, author = "No quote found", "Unknown"
-    else:
-        quote, author = "Error fetching quote", "Unknown"
 
-    return render_template_string('index.html', quote=quote, author=author)
+            # Перевод цитаты с английского на русский
+            try:
+                translated_quote = translator.translate(quote, src='en', dest='ru').text
+                translated_author = translator.translate(author, src='en', dest='ru').text
+            except Exception as e:
+                translated_quote = "Ошибка при переводе"
+                translated_author = "Ошибка при переводе"
+        else:
+            quote, author, translated_quote, translated_author = "No quote found", "Unknown", "No translation available", "Unknown"
+    else:
+        quote, author, translated_quote, translated_author = "Error fetching quote", "Unknown", "No translation available", "Unknown"
+
+    return render_template('index.html', quote=quote, author=author, translated_quote=translated_quote, translated_author=translated_author)
 
 if __name__ == '__main__':
     app.run(debug=True)
